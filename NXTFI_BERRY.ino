2e04c5b62d57916ce26870e6810dd0abb9975ee1
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2022-06-22 08:57:48 +0800

    Add files via upload

--- NXTFI_BERRY.ino
+++ NXTFI_BERRY.ino
@@ -1,14 +1,28 @@
+
+#include <WiFi.h>
+#include <WebServer.h>
+#include "FS.h"
+#include "SPIFFS.h"
 #include <berry.h>
 //#include <be_repl.h>
 #include <be_vm.h>
 
+//berry only is 340651 freeram
+
 #include <Ticker.h>
 
-
+WebServer server(80);
 Ticker tick;
+const char* ssid = "MikroTik0";
+const char* password = "";
+
 
 extern "C" {
   void be_load_baselib(bvm *vm);
+}
+
+void handleTest(){
+  
 }
 
 void memory_check() {
@@ -47,16 +61,34 @@ static const char* get_line(const char *prompt)
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  SPIFFS.begin();
+  server.on("/test",handleTest);
+  server.begin();
+  
+  Serial.printf("PULLUP: %d", PULLUP);
+  Serial.printf("PULLDOWN: %d", PULLDOWN);
   Serial.println("starting vm");
   tick.attach(10, memory_check);
   bvm *vm = be_vm_new();
   be_dostring(vm, "print(\"this is the repl\")");
-  be_load_baselib(vm);
+
   /* // repl has memory leak
     char cmd[] = "print(\"Hello world!\")";
     be_pushstring(vm, cmd);
     int res = be_repl(vm, &get_line);
-
     if (res == -BE_MALLOC_FAIL) {
       be_writestring("error: memory allocation failed.\n");
     }
---
+++ berry_extern.ino
@@ -0,0 +1,8 @@
+
+
+extern "C" {
+  void *berry_malloc32(size_t size);
+  void *berry_malloc32(size_t size) {
+    return NULL;
+  }
+}
